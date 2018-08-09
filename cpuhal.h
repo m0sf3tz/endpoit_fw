0a3229d717aa1cb9b802308aa07ca89ef081c913
sammy <hoshyar@gmail.com>
2018-08-08 23:53:00 -0700

    cleaning up

--- cpuhal.h
+++
@@ -1,6 +0,0 @@
-
-extern RCC_OscInitTypeDef rccHandle;
-
-void initRcc4mhz(void);
-void initRcc65khz(void);
-void initSysTick(void);
--- hal/cpuHal.c
+++ hal/cpuHal.c
@@ -1,6 +1,6 @@
 #include "stm32l0xx_hal_rcc.h"
 #include <stdint.h>
-#include "globals.h"
+
 
 RCC_OscInitTypeDef rccHandle;
 
@@ -11,8 +11,6 @@ void initRcc4mhz()
 	rccHandle.MSIClockRange  	    = RCC_ICSCR_MSIRANGE_6; /*!< MSI = 4.194 MHz   */
 	//rccHandle.MSICalibrationValue = 0x2F;
 	HAL_RCC_OscConfig(&rccHandle);
-	
-
 }
 
 void initRcc65khz()
@@ -22,11 +20,4 @@ void initRcc65khz()
 	rccHandle.MSIClockRange  	    = RCC_ICSCR_MSIRANGE_0; /*!< MSI = 65khz   */
 	//rccHandle.MSICalibrationValue = 0x2F;
 	HAL_RCC_OscConfig(&rccHandle);
-}
-
-void initSysTick()
-{
-	#if DEBUG_MODE
-	  SysTick_Config(4300000/100);      /* Generate interrupt each 1 ms  */
-	#endif
 }
