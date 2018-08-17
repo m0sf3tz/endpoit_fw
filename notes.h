c9fe069158e312ac69ba3e1d7136c4f6e5d63b58
sammy <hoshyar@gmail.com>
2018-08-17 16:51:17 -0700

    fixed up ADC functions

---
+++ notes.h
@@ -0,0 +1,22 @@
+/*
+- To send one sectore will consume 1.28V
+- VCAP reading from ADC of (0x213E == 5.96V)
+- Current FW will begin sampling once we have 5.5V saved up
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
